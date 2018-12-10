84d55d49a37648d93819df62ce7f54cb71545387
Jamil Abbas <jamilabbas787@gmail.com>
2018-12-09 20:57:26 -0500

    Chapter 1 Complete

--- First_Project/First_Project/Startup.cs
+++ First_Project/First_Project/Startup.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using First_Project.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace First_Project
 {
@@ -23,7 +25,7 @@ namespace First_Project
                 .AddJsonFile("appsettings.json");
 
             Configuration = builder.Build();
-            var message = Configuration["Message"];
+
                 
         }
 
@@ -31,11 +33,16 @@ namespace First_Project
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            //services.AddSingleton<IMessageService, HardcodedMessageService>();
+            services.AddSingleton(providers => Configuration);
+            services.AddSingleton<IMessageService, ConfigurationMessageService>();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, IMessageService msg)
         {
+            loggerFactory.AddConsole();
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -43,7 +50,8 @@ namespace First_Project
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                //var message = Configuration["Message"];
+                await context.Response.WriteAsync(msg.GetMessage());
             });
         }
     }
